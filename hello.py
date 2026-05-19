564cfc6ff25dc553a3acbd6442f81de79682afed
jhlw <jhlwada@gmail.com>
2026-05-19 17:07:25 +0800

    首次提交

---
+++ hello.py
@@ -0,0 +1,12 @@
+#This program says hello and asks for my name.
+
+print('Hello world!')
+print('what is your name?') #asking for their name
+myName = input() #taking input from user
+print('It is good to meet you ,'+ myName ) #greeting the user with their name
+print('Thw length of your name is :') #asking for the length of their name
+print(len(myName)) 
+print('What is your age?') #printing the length of their name
+myAge=input()
+print('You will be ' + str(int(myAge) + 1)  +' in a year.') #greeting the user with their age
+
